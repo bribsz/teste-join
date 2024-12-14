cdc6141eda88528a5eaa76ac287e814d9c4c2ca6
bribsz <brenohribeiroh@gmail.com>
2024-12-14 19:01:00 -0300

    Ajustando CORS back-front

---
+++ src/main/java/com/brenoDev/joinTeste/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.brenoDev.joinTeste.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permitir todos os endpoints
+                        .allowedOrigins("http://localhost:4200") // Permitir apenas esta origem
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Permitir todos os cabeçalhos
+                        .allowCredentials(true); // Permitir credenciais (cookies, etc.)
+            }
+        };
+    }
+}
--- src/main/java/com/brenoDev/joinTeste/controllers/ProdutoController.java
+++ src/main/java/com/brenoDev/joinTeste/controllers/ProdutoController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -26,6 +27,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/produtos")
+@CrossOrigin(origins = "http://localhost:4200") // Permitir apenas essa origem
 public class ProdutoController {
 
     @Autowired
@@ -37,7 +39,7 @@ public class ProdutoController {
 		return ResponseEntity.ok().body(obj);
 	}
 	
-	//endpoint -> localhost:8080/livros?categoria=1
+	//endpoint -> localhost:8080/produtos?categoria=1
 	@GetMapping()
 	public ResponseEntity<List<ProdutoDTO>> findAll(@RequestParam(value = "categoria", defaultValue = "0") Long id_cat) {
 		List<Produto> list = produtoService.findAll(id_cat);
